ce50144d142321cea799a77e2eb87a76913c3637
bitgoalsdeveloper <bitgoals.developer@gmail.com>
2022-03-02 13:59:38 -0700

    Add Validate

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # Send Free Ticktes
 
 ## How to run
-
+```
     git clone https://github.com/bitgoalsdeveloper/send_free_tickets.git
     cp .env.example .env
     ## update the key
     npm i
     npx hardhat run --network mainnet scripts/free-tickets.js
+
+```
+## How to run validate
+```
+    npx hardhat run --network mainnet scripts/free-tickets.js
+```
---
+++ scripts/validate-tickets.js
@@ -0,0 +1,38 @@
+const { ethers } = require("hardhat");
+const storage = require('node-persist');
+
+const AMOUNT = ethers.utils.parseEther("0.3"); // PLT
+
+async function main() {
+    const [owner] = await ethers.getSigners();
+    console.log(`==== Running =====`);
+    await storage.init({ dir: './persist',});
+    
+    const pltSmallContractABI = ['function transfer(address recipient, uint256 amount) external returns (bool)',
+                                 'function balanceOf(address account) external view returns (uint256)'
+                                ];
+    const pltContractAddress = '0x631C2f0EdABaC799f07550aEE4fF0Bf7fd35212B';
+    const pltContract = await ethers.getContractAt(pltSmallContractABI, pltContractAddress, owner);
+    
+    const addressList = await storage.keys();
+    var founds = 0
+    for (const address of addressList) {
+        var pltBalance = await pltContract.balanceOf(address);
+        if (pltBalance.eq(AMOUNT) == false) {
+            founds++;
+            console.log(`PLT used by address: ${address}, URL: https://testnet.bscscan.com/address/${address}#tokentxns`)
+        }
+    };
+
+    console.log(`Total: ${addressList.length}, Found: ${founds}, Percentage: ${founds / addressList.length * 100}%`)
+}
+
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
