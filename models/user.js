ebb1787de9ac625f392da14c2a0bc571bb97a576
bitgoalsdeveloper <bitgoals.developer@gmail.com>
2022-03-11 12:46:33 -0700

    add tasks

---
+++ models/user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+/**
+ * User Schema
+ */
+
+const userSchema = new Schema({
+    name: {},
+    address: {},
+    tag: {},
+    comment: {},
+    need_to_send: {},
+    created_at: { type: Date },
+    updated_at: { type: Date }
+
+});
+
+userSchema.pre('save', function (next) {
+    var now = new Date();
+    this.updated_at = now;
+    if (!this.created_at) {
+        this.created_at = now;
+    }
+    next();
+})
+
+
+mongoose.model('User', userSchema);
---
+++ tasks/erc20/sendErc20.js
@@ -0,0 +1,58 @@
+'use strict';
+
+require('../../models/user');
+const logger = require("../../utils/logger.js")
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+const AMOUNT = ethers.utils.parseEther("0.3"); // PLT
+
+class SendERC20Task {
+
+    async run() {
+        logger.info(`==== Running SendERC20Task =====`)
+        await mongoose.connect(process.env.MONGO_DB);
+
+        const [owner] = await ethers.getSigners();
+
+        var users = await User.find({'need_to_send': true}, {});
+
+        const pltSmallContractABI = ['function transfer(address recipient, uint256 amount) external returns (bool)',
+            'function balanceOf(address account) external view returns (uint256)'
+        ];
+        const pltContractAddress = '0x631C2f0EdABaC799f07550aEE4fF0Bf7fd35212B';
+        const pltContract = await ethers.getContractAt(pltSmallContractABI, pltContractAddress, owner);
+
+        for (let user of users) {
+            try {
+
+                logger.info(`==== Sending PLT to ${user.address} =====`);
+                var tx = await pltContract.transfer(user.address, AMOUNT);
+                tx = await tx.wait();
+
+                var now = new Date();
+                const filter = { address: user.address };
+                const update = {
+                   need_to_send: false,
+                   updated_at: now
+                };
+
+                // `doc` is the document _before_ `update` was applied
+                let doc = await User.findOneAndUpdate(filter, update, {
+                    returnNewDocument: true,
+                    new: true,
+                    strict: false
+                });
+
+
+            } catch (e) {
+                console.log(e);
+                logger.error(e)
+            }
+        }
+    }
+}
+
+module.exports = SendERC20Task;
+
+
