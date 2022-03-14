4c313a6e558328898609f65b175af1bae7aa17b5
bitgoalsdeveloper <bitgoals.developer@gmail.com>
2022-03-14 11:52:36 -0600

    Bluk Send

--- tasks/erc20/sendErc20.js
+++ tasks/erc20/sendErc20.js
@@ -3,6 +3,7 @@
 require('../../models/user');
 const logger = require("../../utils/logger.js")
 const mongoose = require('mongoose');
+const { use } = require('chai');
 const User = mongoose.model('User');
 
 class SendERC20Task {
@@ -22,19 +23,32 @@ class SendERC20Task {
         const pltContractAddress = '0x631C2f0EdABaC799f07550aEE4fF0Bf7fd35212B';
         const pltContract = await ethers.getContractAt(pltSmallContractABI, pltContractAddress, owner);
 
+        const airDropSmallContractABI = ['function send(address token, address[] calldata addresses, uint256 [] calldata values) external returns(uint256)'];
+        const airDropContractAddress = '0xdeCDf579Ef60D7F5435701f5dC01a52764244948';
+        const airDropContract = await ethers.getContractAt(airDropSmallContractABI, airDropContractAddress, owner);
 
         var users = await User.find({ 'erc20_sent': false }, {});
         logger.info(`==== Users to send count: ${users.length} =====`);
 
-        var sentCount = 0;
+        var addressesToSent = [];
+        var amountToSent = [];
+        users.map(user => {
+            addressesToSent.push(user.address);
+            amountToSent.push(AMOUNT);
+        })
+
+        try {
+            var tx = await airDropContract.send(pltContract.address, addressesToSent, amountToSent);
+            tx = await tx.wait();
+
+            logger.info(`==== PLT Sent Count:${addressesToSent.length}, URL: https://bscscan.com/tx/${tx.hash} =====`);
+        } catch(e) {
+            console.log(e);
+        }
+
         for (let user of users) {
             try {
                 
-                var tx = await pltContract.transfer(user.address, AMOUNT);
-                tx = await tx.wait();
-
-                logger.info(`==== PLT Sent to ${user.address} =====`);
-                sentCount++
                 var now = new Date();
                 const filter = { address: user.address };
                 const update = {
@@ -51,11 +65,9 @@ class SendERC20Task {
 
 
             } catch (e) {
-                //logger.error(e)
+                console.log(e)
             }
         }
-
-        logger.info(`==== Total Sent:  ${sentCount} =====`);
     }
 }
 
